62711caca06414412d72eff2db6d6330a47e22d2
David Kendlbacher <dg.kendlbacher@gmail.com>
2018-09-07 21:03:50 +0200

    authentication before bcrypt

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+
 const Post = require("../models/post");
+const User = require("../models/user");
+
 const router = express.Router();
+
+router.post("/signup", (req, res, next) => {
+  const user = new User({
+    email: req.body.email;
+
+  });
+});
 
 module.exports = router;
