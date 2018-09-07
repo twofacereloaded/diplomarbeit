9842fcdd799270af71f9305df64da7f8215afe3d
David Kendlbacher <dg.kendlbacher@gmail.com>
2018-09-07 22:03:52 +0200

    login and signup before spa auth

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,9 +9,23 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
-  const user = new User({
-    email: req.body.email;
-
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+    user.save()
+      .then(result => {
+        res.status(201).json({
+          message: 'User created',
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: err
+        });
+      });
   });
 });
 
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -8,10 +9,13 @@ import { NgForm } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
   isLoading = false;
-  constructor() {}
+  constructor(public authService: AuthService) {}
 
-  onLogin(form: NgForm) {
-    console.log(form.value);
+  onSignup(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.authService.createUser(form.value.email, form.value.password);
   }
 
   ngOnInit() {}
