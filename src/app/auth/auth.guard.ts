eef4f79a0aae0dd58b251bf2cd4bae88b044d1e8
David Kendlbacher <dg.kendlbacher@gmail.com>
2018-09-08 14:49:32 +0200

    add timeout token

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -20,7 +20,7 @@ export class AuthGuard implements CanActivate {
     const isAuth = this.authService.getIsAuth();
     if (!isAuth) {
 
-      this.router.navigate(['/']);
+      this.router.navigate(['/login']);
     }
     return isAuth;
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -10,6 +10,7 @@ import { AuthData } from './auth-data.model';
 export class AuthService {
   private token: string;
   private isAuthenticated = false;
+  private tokenTimer;
   private authStatusListener = new Subject<boolean>();
   constructor(private http: HttpClient, private router: Router) {}
 
@@ -34,11 +35,16 @@ export class AuthService {
 
   login(email: string, password:string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+    this.http.post<{token: string, expiresIn: number}>('http://localhost:3000/api/user/login', authData)
     .subscribe(response => {
       const token = response.token;
       this.token = token;
       if (token) {
+        const expiresInDuration = response.expiresIn;
+        console.log(expiresInDuration);
+        this.tokenTimer = setTimeout(() =>  {
+          this.logout();
+        }, expiresInDuration * 1000);
         this.isAuthenticated = true;
         this.authStatusListener.next(true);
         this.router.navigate(['/']);
@@ -50,6 +56,7 @@ export class AuthService {
     this.token = null;
     this.isAuthenticated = false;
     this.authStatusListener.next(false);
-    this.router.navigate(["/"]);
+    this.router.navigate(['/']);
+    clearTimeout(this.tokenTimer);
   }
 }
