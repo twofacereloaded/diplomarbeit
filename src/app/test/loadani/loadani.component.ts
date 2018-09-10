8573084f288bb95c33a479d69d33db54dd5a6210
David Kendlbacher <dg.kendlbacher@gmail.com>
2018-09-10 12:32:11 +0200

    add underscore test

--- src/app/test/loadani/loadani.component.ts
+++ src/app/test/loadani/loadani.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import * as _ from 'underscore';
-import * as _ from 'lodash';
+import * as _ from 'underscore';
+import * as __ from 'lodash';
 
 declare var jsSHA: any;
 
@@ -12,17 +12,15 @@ declare var jsSHA: any;
 export class LoadaniComponent implements OnInit {
 
   shaObj: any;
-  hash: string;
-  randomNum: number;
-
+  
   constructor() {
-    // const myArray: number[] = [9, 1, 5];
-    // const lastItem: number = _.last(myArray); // Using underscore
-    // console.log(lastItem); // 5
+    const myArray: number[] = [9, 1, 5];
+    const lastItem: number = _.last(myArray); // Using underscore
+    console.log(lastItem); // 5
     this.shaObj = new jsSHA("SHA-512", "TEXT");
     this.shaObj.update("This is a test");
-    this.hash = this.shaObj.getHash("HEX");
-    this.randomNum = _.random(1, 100);
+    console.log(this.shaObj.getHash("HEX"));
+    console.log(__.random(1, 100));
   }
 
   ngOnInit() {}
